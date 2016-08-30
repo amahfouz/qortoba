eed0859acf7c7bbb4941a6c5723600f03a1cd42f
Ayman Mahfouz <amahfouz@webalo.com>
2016-08-30 12:56:19 -0700

    Added Android implementation.

---
+++ java/com/mahfouz/qortoba/AndroidQortobaClient.java
@@ -0,0 +1,52 @@
+package com.mahfouz.qortoba;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.util.Log;
+import android.webkit.WebView;
+
+import com.mahfouz.qortoba.internal.QortobaInvocation;
+
+/**
+ * Android-specific Qortoba client.
+ * 
+ * Invokes arbitrary angular service method on an Android WebView.
+ */
+public final class AndroidQortobaClient {
+
+	private static final String LOG_TAG = "QORTOBA";
+	
+	private final WebView webView;
+	private final AndroidWebView wrapper;
+	
+	public AndroidQortobaClient(WebView webView) {
+		if (webView == null)
+			throw new IllegalArgumentException();
+		
+		this.webView = webView;
+		this.wrapper = new AndroidWebView();
+	}
+
+	public void invoke(String serviceName, String methodName, JSONArray params) {
+		QortobaInvocation inv = new QortobaInvocation
+			(serviceName, methodName, params != null ? params.toString() : null);
+		inv.invoke(wrapper);
+	}
+	
+
+	/**
+	 * Wraps an Android WebView for passing to a QortobaInvocation.
+	 */	
+	private final class AndroidWebView implements QortobaInvocation.WebView {
+
+		@Override
+		public void runJavaScript(String jsString) {
+			
+S			// add a void(0); to force the JS to execute
+			
+			String augmentedJs = "javascript:" + jsString + "void(0);";
+			webView.loadUrl(augmentedJs);
+		}
+	}
+}
---
+++ java/com/mahfouz/qortoba/internal/QortobaInvocation.java
@@ -0,0 +1,62 @@
+package com.mahfouz.qortoba.internal;
+
+/**
+ * Platform-independent invocation of Qortoba.
+ * 
+ * Invocation specified using an (angular) service name, a method 
+ * name (of the service) and params (specified as an array serialized
+ * as a String in the form "[param1, param2, param3]").
+ */
+public final class QortobaInvocation {
+
+	private static final String SERVICE_PLACEHOLDER = "wl-service_name";
+	private static final String METHOD_PLACEHOLDER = "wl-method_name";
+	private static final String PARAMS_PLACEHOLDER = "wl-params_array";
+
+	private static final String INVOCATION_JS_STR 
+		= "qortoba.callback(1.0, \"wl-service_name\", \"wl-method_name\", \"wl-params_array\");";
+
+	private final String serviceName;
+	private final String methodName;
+	private final String paramsArrStr;
+
+	public QortobaInvocation(String serviceName, 
+							 String methodName, 
+							 String paramsArrIfAny) {
+
+		if (serviceName == null || methodName == null)
+			throw new IllegalArgumentException();
+
+		this.serviceName = serviceName;
+		this.methodName = methodName;
+		this.paramsArrStr = paramsArrIfAny;
+	}
+
+	public void invoke(WebView webView) {
+		
+		// replace placholders with actual values
+		
+		String withService = INVOCATION_JS_STR.replace(SERVICE_PLACEHOLDER, serviceName);
+		String withMethod = withService.replace(METHOD_PLACEHOLDER, methodName);
+		
+		String serializedParams = (paramsArrStr != null)
+			? paramsArrStr
+			: "[]";
+		
+		// escape the double quotes
+		String escapedParams = serializedParams.replace("\"", "\\\"");
+		String withParams = withMethod.replace(PARAMS_PLACEHOLDER, escapedParams);
+
+		// run the JavaScript on the web view
+		
+		webView.runJavaScript(withParams);
+	}
+
+	//
+	// Abstracts a web view
+	//
+
+	public interface WebView {
+		void runJavaScript(String jsString);
+	}
+}

50131a718b88bbdf911ed0ccc98cd7d7079ad4b4
Ayman Mahfouz <amahfouz@webalo.com>
2016-08-30 15:33:31 -0700

    Got Android version working and added logging.

--- js/qortoba-android.js
+++ js/qortoba-android.js
@@ -3,27 +3,35 @@
 // Android specific service to call Android code from JavaScript
 //
 
+angular.module('qortoba', [])
 
-// Expect the 'qortoba' module to be already defined
-
-angular.module('qortoba')
-
-.factory('Qortoba', ['$log', function($log) {
+.factory('QortobaService', ['$log', function($log) {
 	
-	// service API
+	// Service API
 
 	return {
 		
 		exec : function(service, action, argsArr) {
 			
 			// Android code can inject an object for each service
-
+			
 			var injectedJsApi = window[service];
+			
+			if (! injectedJsApi) {
+				$log("QORTOBA: No injected object named " + service + ".");
+				return;
+			}
+			
 			var fun = injectedJsApi[action];
-
+			
+			if (! fun) {
+				$log("QORTOBA: No method named " + action + ".");
+				return;
+			}
+			
 			// invoke the method
 
-			fun.apply(injectedJsApi, args)
+			fun.apply(injectedJsApi, argsArr)
 		}
 	};
 }]);

4ddcf87c5e89f6d50a6fb485079ca2a49318add5
Ayman Mahfouz <amahfouz@gmail.com>
2016-09-23 00:53:37 -0700

    Added docs to the example service. Modified xcode project.

--- src/js/example-service.js
+++ src/js/example-service.js
@@ -1,13 +1,29 @@
+//
+// Angular module and service used for demonstration
+//
+// It is supposed to be called from outside 
+// JavaScript using Qortoba.callback function
+//
+// When called, it simply forwards the call
+// back to the native code using the exec()
+// function of the QortobaService
+//
+
 angular.module("example", ['qortoba'])
+
+//
+// Functions of the example service simply forward calls to
+// the native methods of the same name.
+//
 
 .factory("ExampleService", function(QortobaService) {
 	
 	return {
 		action1 : function() {
-			QortobaService.exec("sample", "action1:", null);
+			QortobaService.exec("sample", "action1", null);
 		},
 		action2 : function(param1, param2) {
-			QortobaService.exec("sample", "action2:withParam2:", [param1, param2]);
+			QortobaService.exec("sample", "action2", [param1, param2]);
 		}		
 	};
 });

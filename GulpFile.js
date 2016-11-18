2dce012800f0a1a9d166689676fe5c38f3683263
Ayman Mahfouz <amahfouz@gmail.com>
2016-11-17 23:16:39 -0800

    Added gulp-javac and corresponding task.

--- GulpFile.js
+++ GulpFile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 
+javac = require('gulp-javac');
+
 /*
  * Copies the js files and index.html to the folder
  * linked to the xcode project.
 */
-gulp.task('default', function() {
+gulp.task('web-copy', function() {
     gulp.src('./src/js/*').
         pipe(gulp.dest('./build/www'));
     gulp.src('./example/web/*').
         pipe(gulp.dest('./build/www'));
+});
+
+gulp.task('javac-tools', function() {
+   gulp.src('./src/android/com/mahfouz/qortoba/tools/*.java').
+      pipe(javac.javac().addLibraries('')).
+      pipe(javac.jar('qortoba-tools.jar')).
+      pipe(gulp.dest('/Users/amahfouz/code/qortoba/build/java'));
 });
